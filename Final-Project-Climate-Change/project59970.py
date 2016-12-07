e18812c36c503bf5e3ddf7b8d8225d788178e584
brandonmchin <brandonmchin21@gmail.com>
2016-12-07 04:05:34 -0500

    add seasons methods

--- Final-Project-Climate-Change/project59970.py
+++ Final-Project-Climate-Change/project59970.py
@@ -1,9 +1,11 @@
 ##########################################################################################################
 # TABLE OF CONTENTS
 #
-# MODULES / SETUP
-# BASIC TOOLS
-# ANNUAL GLOBAL TEMPERATURES
+# 1.  MODULES / SETUP
+# 2.  BASIC TOOLS
+# 3.  ANNUAL GLOBAL TEMPERATURES
+# 4.  UNCERTAINTY
+# 5.  SEASONS
 
 
 ##########################################################################################################
@@ -29,6 +31,7 @@ def open_file():
 	filename = fd.askopenfilename()
 	return filename
 
+
 def create_dataframe():
 	filename = open_file()
 	return pd.read_csv(filename, parse_dates=['dt'])
@@ -37,7 +40,7 @@ def create_dataframe():
 ##########################################################################################################
 # ANNUAL GLOBAL TEMPERATURES
 
-def get_avg_temperatures(dataframe, start_year, end_year):
+def get_avg_global_temperatures(dataframe, start_year, end_year):
 
 	df = dataframe.copy()
 
@@ -47,8 +50,29 @@ def get_avg_temperatures(dataframe, start_year, end_year):
 	# extract observations of specified year range
 	data = df[(df['dt']>=start_year) & (df['dt']<=end_year)]
 
-	# return numpy array of average temperatures
+	# return numpy array of average global temperatures
 	return data.groupby(['dt']).mean()['AverageTemperature'].values
+
+
+def plot_avg_global_temperatures(dataframe, start_year, end_year):
+
+	# extract corresponding dates and temperatures 
+	dates = np.arange(start_year, end_year + 1)
+	temps = get_avg_global_temperatures(dataframe, start_year, end_year)
+
+	# plot data
+	plt.figure(figsize=(12, 6))
+	plt.scatter(dates, temps, c=temps, s=150, alpha=0.6, edgecolors='none', cmap='viridis')
+	plt.xlim([start_year - 5, end_year + 5])
+	plt.grid(True)
+	plt.title("Average Global Temperatures [" + str(start_year) + ", " + str(end_year) + "]")
+	plt.xlabel("Year")
+	plt.ylabel("Temperature (Celsius)")
+	plt.show()
+
+
+##########################################################################################################
+# UNCERTAINTY
 
 def get_uncertainty(dataframe, start_year, end_year):
 
@@ -64,28 +88,11 @@ def get_uncertainty(dataframe, start_year, end_year):
 	return data.groupby(['dt']).mean()['AverageTemperatureUncertainty'].values
 
 
-def plot_avg_global_temperatures(dataframe, start_year, end_year):
-
-	# extract corresponding dates and temperatures 
-	dates = np.arange(start_year, end_year + 1)
-	temps = get_avg_temperatures(dataframe, start_year, end_year)
-
-	# plot data
-	plt.figure(figsize=(12, 6))
-	plt.scatter(dates, temps, c=temps, s=150, alpha=0.6, edgecolors='none', cmap='viridis')
-	plt.xlim([start_year - 5, end_year + 5])
-	plt.grid(True)
-	plt.title("Average Global Temperatures [" + str(start_year) + ", " + str(end_year) + "]")
-	plt.xlabel("Year")
-	plt.ylabel("Temperature (Celsius)")
-	plt.show()
-
-
 def plot_uncertainty(dataframe, start_year, end_year):
 
 	# extract corresponding dates, temperatures, and uncertainties 
 	dates = np.arange(start_year, end_year + 1)
-	temps = get_avg_temperatures(dataframe, start_year, end_year)
+	temps = get_avg_global_temperatures(dataframe, start_year, end_year)
 	uncertainty = get_uncertainty(dataframe, start_year, end_year)
 
 
@@ -103,3 +110,62 @@ def plot_uncertainty(dataframe, start_year, end_year):
 	plt.ylabel("Temperature (Celsius)")
 	plt.show()
 	
+
+##########################################################################################################
+# SEASONS
+
+def get_season(month):
+	if month >= 3 and month <= 5:
+		return 'Spring'
+	elif month >= 6 and month <= 8:
+		return 'Summer'
+	elif month >= 9 and month <= 11:
+		return 'Autumn'
+	else:
+		return 'Winter'
+
+
+def get_avg_seasonal_global_temperatures(dataframe, start_year, end_year):
+
+	df = dataframe.copy()
+
+	df['Year'] = df['dt'].apply(lambda x: x.year)
+	df['Month'] = df['dt'].apply(lambda x: x.month)
+	df['Season'] = df['Month'].apply(get_season)
+
+	years = np.arange(start_year, end_year + 1)
+
+	spring = []
+	summer = []
+	autumn = []
+	winter = []
+
+	for year in years:
+		data = df[df['Year'] == year]
+		spring.append(data[data['Season']=='Spring']['AverageTemperature'].mean())
+		summer.append(data[data['Season']=='Summer']['AverageTemperature'].mean())
+		autumn.append(data[data['Season']=='Autumn']['AverageTemperature'].mean())
+		winter.append(data[data['Season']=='Winter']['AverageTemperature'].mean())
+
+	return (spring, summer, autumn, winter)
+
+
+def plot_avg_seasonal_global_temperatures(dataframe, start_year, end_year):
+
+    # extract corresponding dates and seasonal data
+	years = np.arange(start_year, end_year + 1)
+	spring, summer, autumn, winter = get_avg_seasonal_global_temperatures(dataframe, start_year, end_year)
+
+	plt.figure(figsize=(12, 6))
+	plt.plot(years, spring, label='Spring', color='sage')
+	plt.plot(years, summer, label='Summer', color='lightcoral')
+	plt.plot(years, autumn, label='Autumn', color='orange')
+	plt.plot(years, winter, label='Winter', color='lightskyblue')
+
+	plt.legend(loc='best')
+	plt.grid(True)
+	plt.xlim(start_year - 5, end_year + 5)
+	plt.title("Average Global Temperatures by Season [" + str(start_year) + ", " + str(end_year) + "]")
+	plt.xlabel('Years')
+	plt.ylabel('Temperature (Celsius)')
+	plt.show()
